ba68f88d10059920f43578fd225f2def26ff05f5
kevin kalenga <kevinkalenga10@gmail.com>
2025-02-26 02:02:35 +0100

    Création du token

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\ResetPassword;
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Mailer\MailerInterface;
-
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 final class SecurityController extends AbstractController
 {
@@ -68,4 +71,37 @@ final class SecurityController extends AbstractController
     }
     #[Route('/logout', name: 'logout')]
     public function logout() {}
+
+
+    #[Route('/reset-password-request', name: 'reset-password-request')]
+    public function resetPasswordRequest(Request $request, UserRepository $userRepository)
+    {
+
+        $emailForm = $this->createFormBuilder()->add('email', EmailType::class, [
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Veuillez renseigner votre email'
+                ])
+            ]
+        ])->getForm();
+
+        $emailForm->handleRequest($request);
+        if ($emailForm->isSubmitted() && $emailForm->isValid()) {
+            $email = $emailForm->get('email')->getData();
+            $user = $userRepository->findOneBy(['email' => $email]);
+            if ($user) {
+                $resetPassword = new ResetPassword();
+                $resetPassword->setUser($user);
+                $resetPassword->setExpiredAt(new \DateTimeImmutable('+2 hours'));
+                $token = substr(str_replace(['+', '/', '='], '', base64_encode(random_bytes(30))), 0, 20);
+                $resetPassword->setToken($token);
+                dump($resetPassword);
+            }
+            $this->addFlash('success', 'Un email vous a été envoyé pour réinitialiser votre mot de passe');
+        }
+
+        return $this->render('security/reset-password-request.html.twig', [
+            'form' => $emailForm->createView()
+        ]);
+    }
 }
